0d5ca82f2e7011f15bc01a15aaf74252952ee5a4
Fazliddin <dilshodovfazliddin@gmail.com>
2026-01-14 15:23:06 +0500

    Initial commit

---
+++ src/main/java/uzumtech/court/jcourtservice/JCourtServiceApplication.java
@@ -0,0 +1,13 @@
+package uzumtech.court.jcourtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCourtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JCourtServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uzumtech/court/jcourtservice/JCourtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uzumtech.court.jcourtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JCourtServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
